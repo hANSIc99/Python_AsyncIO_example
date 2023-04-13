ffd1f348814d2e5b866535dee6e909de150cdd73
Stephan Avenwedde <StephanA@beckhoff.com>
2023-04-13 07:16:13 +0200

    Complete

---
+++ main.py
@@ -0,0 +1,47 @@
+import time
+import asyncio
+import aiohttp
+
+
+async def performGetRequest(session, url):
+    async with session.get('https://httpbin.org/get') as response:
+        html = await response.text()
+        if response.status == 200:
+            return 'Successfully loaded {}'.format(url)
+        else:
+            return 'Error loading {}'.format(url)
+
+
+async def main():
+    
+    print('Start Async I/O Demo')
+
+    myFavorateWebsites = [
+        'https://opensource.com',
+        'https://python.org',
+        'https://github.com',
+        'https://en.cppreference.com'
+        'https://getfedora.org'
+    ]
+
+    async with aiohttp.ClientSession() as session:
+
+        start = time.time()
+        for url in myFavorateWebsites:
+            result = await performGetRequest(session, url)
+            print(result)
+        end = time.time()
+        print('>>> Elapsed time on synchronous access: {}'.format(end - start))
+    
+
+
+        start = time.time()
+        futures = [performGetRequest(session, url) for url in myFavorateWebsites]
+        status = await asyncio.gather(*futures)
+        
+        print('\n'.join(status))
+        end = time.time()
+        print('>>> Elapsed time on asynchronous access: {}'.format(end - start))
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(main())

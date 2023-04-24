7151dab144679508d564ef7f6a177b815218d460
Stephan Avenwedde <StephanA@beckhoff.com>
2023-04-24 08:54:33 +0200

    exception handling added

--- main.py
+++ main.py
@@ -17,31 +17,27 @@ async def main():
     print('Start Async I/O Demo')
 
     myFavorateWebsites = [
-        'https://opensource.com',
+        'https://openssssssource.com', # will cause an exception
         'https://python.org',
         'https://github.com',
         'https://en.cppreference.com',
         'https://getfedora.org'
     ]
 
-    connector = aiohttp.TCPConnector(verify_ssl=False)
+    connector = aiohttp.TCPConnector(ssl=False)
     async with aiohttp.ClientSession(connector=connector) as session:
 
-        start = time.time()
-        for url in myFavorateWebsites:
-            result = await performGetRequest(session, url)
-            print(result)
-        end = time.time()
-        print('>>> Elapsed time on synchronous access: {}'.format(end - start))
-
-        start = time.time()
         tasks = [asyncio.create_task(performGetRequest(session, url)) for url in myFavorateWebsites]
-        await asyncio.gather(*tasks)
+        await asyncio.gather(*tasks, return_exceptions=True)
         
-        results = [task.result() for task in tasks]
+        results = []
+        for task in tasks:
+            try:
+                results.append(task.result())
+            except Exception as e:
+                print('Exception occured: {}'.format(e))
+
         print('\n'.join(results))
-        end = time.time()
-        print('>>> Elapsed time on asynchronous access: {}'.format(end - start))
 
 loop = asyncio.get_event_loop()
 loop.run_until_complete(main())
